1962c06d814e8a0aaf999e90c17107aebcf53449
Luminda-lakmal <luckmald98@gmail.com>
2024-01-31 18:46:14 +0530

    getquiz questions and get score

--- quiz-service/src/main/java/org/example/quizservice/service/QuizService.java
+++ quiz-service/src/main/java/org/example/quizservice/service/QuizService.java
@@ -31,26 +31,12 @@ public class QuizService {
         return new ResponseEntity<>("success", HttpStatus.CREATED);
     }
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
-//        Optional<Quiz> quiz = quizDao.findById(id);
-//        List<Question> questionDB = quiz.get().getQuestions();
-        List<QuestionWrapper> questionsForUser = new ArrayList<>();
-//        for (Question q : questionDB){
-//            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(),q.getOption4());
-//            questionsForUser.add(qw);
-//        }
-        return new ResponseEntity<>(questionsForUser,HttpStatus.OK);
+        Quiz quiz = quizDao.findById(id).get();
+        List<Integer> questionIds = quiz.getQuestionIds();
+        return quizInterface.getQuestionsFromId(questionIds);
     }
     public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
-        Quiz quiz = quizDao.findById(id).get();
-//        List<Question> questions = quiz.getQuestions();
-      int right = 0;
-//        int i = 0;
-//        for(Response response : responses){
-//            if(response.getResponse().equals(questions.get(i).getRightAnswer()))
-//                right++;
-//
-//            i++;
-//        }
-        return new ResponseEntity<>(right, HttpStatus.OK);
+        ResponseEntity<Integer> score = quizInterface.getScore(responses);
+        return score;
     }
 }
